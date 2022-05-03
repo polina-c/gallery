30bf50943541d24a94d7df69a38a0b8dd11ae02a
Polina Cherkasova <polina.c@live.com>
2022-05-03 11:15:34 -0700

    -

--- test/restoration_test.dart
+++ test/restoration_test.dart
@@ -6,9 +6,18 @@ import 'package:gallery/pages/demo.dart';
 import 'package:gallery/pages/home.dart';
 import 'package:gallery/studies/reply/app.dart';
 import 'package:gallery/studies/reply/search_page.dart';
-import 'package:memory_tools/leak_detector.dart' as leak_detector;
+import 'package:memory_tools/test_leak_detector.dart' as leak_detector;
 
 void main() {
+  setUpAll(() {
+    leak_detector.init(
+        timeToGC: const Duration(seconds: 2, minutes: 0), detailedOutput: true);
+  });
+
+  tearDownAll(() async {
+    await leak_detector.wrapUp();
+  });
+
   testWidgets(
     'State restoration test - Home Page',
     (tester) async {
@@ -54,8 +63,6 @@ void main() {
       await tester.restartAndRestore();
       await tester.pump(const Duration(seconds: 1));
       expect(find.byKey(const ValueKey('app-bar@material')), findsOneWidget);
-
-      await leak_detector.forceGC();
     },
     variant: const TargetPlatformVariant(
       <TargetPlatform>{TargetPlatform.android},
